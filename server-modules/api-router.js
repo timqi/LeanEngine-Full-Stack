399c69222c8c0393339e63ceb5cd7a6c4f77dfd9
尼奥 <1988wangxiao@gmail.com>
2015-11-18 17:59:10 +0800

    [server] 引入 babel 支持 async/await 。

--- server-modules/api-router.js
+++ server-modules/api-router.js
@@ -17,4 +17,20 @@ const hello = require('./hello');
 // 一个 API 路由下的 hello 接口，访问 /api/hello
 router.get('/hello', hello.hello);
 
+// 测试 async/await 支持
+// const f = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(123);
+//     }, 2000);
+//   });
+// };
+
+// const testAsync = async () => {
+//   const t = await f();
+//   console.log(t);
+// };
+
+// testAsync();
+
 module.exports = router;
