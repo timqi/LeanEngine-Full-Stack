77d4321970be6607ddfde5fd23bd1d185b284dc7
HeZiQiang <heziqiang@qq.com>
2016-01-12 22:28:11 +0800

    new LeanEngine domain
    change from avosapps.com to leanapp.cn

--- server-modules/config.js
+++ server-modules/config.js
@@ -22,8 +22,8 @@ let config = {
     'http://localhost:9000',
     'http://localhost:3000',
     // 以下两个是在 LeanCloud 中配置的 host，xxx 替换为自己的域名
-    'http://dev.xxx.avosapps.com',
-    'http://xxx.avosapps.com'
+    'http://stg-xxx.leanapp.cn',
+    'http://xxx.leanapp.cn'
   ]
 };
 
@@ -32,14 +32,14 @@ switch (process.env.LC_APP_ENV) {
 
   // 当前环境为线上测试环境
   case 'stage':
-    config.host = 'http://dev.xxx.avosapps.com';
-    config.webHost = 'http://dev.xxx.avosapps.com';
+    config.host = 'http://stg-xxx.leanapp.cn';
+    config.webHost = 'http://stg-xxx.leanapp.cn';
   break;
 
   // 当前环境为线上正式运行的环境
   case 'production':
-    config.host = 'http://xxx.avosapps.com';
-    config.webHost = 'http://xxx.avosapps.com';
+    config.host = 'http://xxx.leanapp.cn';
+    config.webHost = 'http://xxx.leanapp.cn';
   break;
 }
 
